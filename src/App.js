c2f9b50bc58079c765d7fb135cb43e052d9fc8ec
David Anderson <david.anderson1993@gmail.com>
2021-06-09 20:32:42 -0500

    saving progress of app. styled page and basic functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/app.scss";
 //ADDING COMPONENTS//
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
+
+import data from "./util";
 
 function App() {
+  //STATE//
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+      />
+      <Library songs={songs} /> 
     </div>
   );
 }
---
+++ src/util.js
@@ -0,0 +1,81 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Sanity",
+      cover:
+        "https://pbs.twimg.com/media/E06gRHbX0AIcZkd?format=jpg&name=large",
+      artist: "David Anderson",
+      audio: "https://davidanderson76.github.io/DavidAnderson/music/sanity.wav",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Human",
+      cover:
+        "https://pbs.twimg.com/media/EdzUBBOX0AIZPkX?format=jpg&name=medium",
+      artist: "David Anderson",
+      audio: "https://davidanderson76.github.io/DavidAnderson/music/human.wav",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: " The World ",
+      cover:
+        "https://pbs.twimg.com/media/EdzUBBQWoAEr4ri?format=jpg&name=large",
+      artist: "David Anderson",
+      audio:
+        "https://davidanderson76.github.io/DavidAnderson/music/hatetheworld.wav",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Round N' Round",
+      cover:
+        "https://pbs.twimg.com/media/EdzUBBPXoAE1cdT?format=jpg&name=medium",
+      artist: "David Anderson",
+      audio:
+        "https://davidanderson76.github.io/DavidAnderson/music/round N' round.wav",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Schmuggly",
+      cover:
+        "https://pbs.twimg.com/media/EdzUBBSWAAAbTCw?format=jpg&name=medium",
+      artist: "David Anderson",
+      audio:
+        "https://davidanderson76.github.io/DavidAnderson/music/schmuggly.wav",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Sorry",
+      cover:
+        "https://pbs.twimg.com/media/EdzUBBSWAAAbTCw?format=jpg&name=medium",
+      artist: "David Anderson",
+      audio: "https://davidanderson76.github.io/DavidAnderson/music/sorry.wav",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Ugly",
+      cover:
+        "https://pbs.twimg.com/media/EdzUBBSWAAAbTCw?format=jpg&name=medium",
+      artist: "David Anderson",
+      audio: "https://davidanderson76.github.io/DavidAnderson/music/ugly.wav",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
